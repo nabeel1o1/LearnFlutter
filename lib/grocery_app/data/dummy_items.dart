af243badd3f4352fc46aae7852575e70cee5c4ef
nabeel <nabeel101ahmed@gmail.com>
2024-07-31 02:53:50 +0500

    Initialized Grocery App

---
+++ lib/grocery_app/data/dummy_items.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutteroid_app/grocery_app/data/categories.dart';
+import 'package:flutteroid_app/grocery_app/models/grocery_item.dart';
+
+const groceryItems = [
+  GroceryItem(
+      id: 'a',
+      name: 'Milk',
+      quantity: 1,
+      category: categories[Categories.dairy]!),
+  GroceryItem(
+      id: 'b',
+      name: 'Bananas',
+      quantity: 5,
+      category: categories[Categories.fruit]!),
+  GroceryItem(
+      id: 'c',
+      name: 'Beef Steak',
+      quantity: 1,
+      category: categories[Categories.meat]!),
+];
